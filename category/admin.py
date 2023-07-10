245ac8f867d770e3d06ad5ddcd69aebb150ed5f9
kev-n14 <kevinfinn688@gmail.com>
2023-07-10 20:34:26 +0000

    add models to category app, add items to categories,create a store app

--- category/admin.py
+++ category/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Category
 
-admin.site.register(Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['category_name', 'slug']
+    prepopulated_fields = {'slug': ('category_name',)}
+
+admin.site.register(Category, CategoryAdmin)
--- primepulse/settings.py
+++ primepulse/settings.py
@@ -47,6 +47,11 @@ INSTALLED_APPS = [
     'cloudinary',
     'category',
     'accounts',
+
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -76,6 +81,17 @@ TEMPLATES = [
         },
     },
 ]
+AUTHENTICATION_BACKENDS = [
+  
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+
+]
+SITE_ID = 1
+
 
 WSGI_APPLICATION = 'primepulse.wsgi.application'
 
@@ -132,6 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 STATIC_URL = '/static/'
