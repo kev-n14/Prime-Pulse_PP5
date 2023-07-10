63bee0d01de3ee83c499d8c4053c12b86c9585c2
kev-n14 <kevinfinn688@gmail.com>
2023-07-10 23:11:11 +0000

    add context_processors file to category app, linked for navbar dropdown menu, fix store button error

--- category/models.py
+++ category/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 
 class Category(models.Model):
     category_name = models.CharField(max_length=50, unique=True)
@@ -10,6 +10,9 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'category'
         verbose_name_plural = 'categories'
+
+    def get_url(self):
+        return reverse('products_by_category', args=[self.slug])
 
     def __str__(self):
         return self.category_name
--- store/urls.py
+++ store/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.store, name='store'),
-    path('<slug:category_slug>/', views.store, name='store'),
+    path('<slug:category_slug>/', views.store, name='products_by_category'),
     
 ] 
