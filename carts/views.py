e9de7db64635e5d3160118bec553e8296157f59e
kev-n14 <kevinfinn688@gmail.com>
2023-07-12 13:56:50 +0000

    fix page number buttons and shopping cart button when empty

--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product
 from carts.models import Cart, CartItem
-
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 
 
@@ -57,8 +57,10 @@ def remove_cart_item(request, product_id):
 
 
 
-def cart(request, total=0, quantity=0):
+def cart(request, total=0, quantity=0, cart_items=None):
     try:
+        tax = 0
+        grand_total = 0
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
