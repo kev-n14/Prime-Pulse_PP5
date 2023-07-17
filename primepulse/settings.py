2cdfe897ae6f58f947e97f17882a71518b8f0b85
kev-n14 <kevinfinn688@gmail.com>
2023-07-17 04:53:52 +0000

    Added cache control

--- primepulse/settings.py
+++ primepulse/settings.py
@@ -164,6 +164,11 @@ STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
 if 'USE_AWS' in os.environ:
+     AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'prime-pulse'
     AWS_S3_REGION_NAME = 'EU (Ireland) eu-west-1'
