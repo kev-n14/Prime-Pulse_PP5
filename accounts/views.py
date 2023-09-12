bc572eed245893319333bda7bc26e9811cb6f74f
kev-n14 <kevinfinn688@gmail.com>
2023-09-12 17:23:28 +0000

    fix 500 error appearing

--- accounts/views.py
+++ accounts/views.py
@@ -54,7 +54,7 @@ def register(request):
             mail_subject = 'Please activate your account'
             message = render_to_string('accounts/account_verification_email.html', {
                 'user': user,
-                'domain': current_site,
+                'domain': 'https://prime-pulse-d780887a098c.herokuapp.com/',
                 'uid': urlsafe_base64_encode(force_bytes(user.pk)),
                 'token': default_token_generator.make_token(user),
             })
