491d22767b0e1592da82a454802f71a0d65dd10c
kev-n14 <kevinfinn688@gmail.com>
2023-09-12 20:54:32 +0000

    update newsletter email

--- primepulse/settings.py
+++ primepulse/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = 'DEVELOPMENT' in os.environ#
+DEBUG = 'DEVELOPMENT' in os.environ
 #DEBUG = True
 #DEBUG = os.environ.get("DEBUG", False)
 
